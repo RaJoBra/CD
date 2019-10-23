d72c954e7c966572c0d7a0608ef60f3c85ac07fb
Raphael Brand <raphael.brand@gmx.net>
2019-10-23 18:32:32 +0200

    request-handler.ts fertig

--- src/server/shared/request-handler.ts
+++ src/server/shared/request-handler.ts
@@ -1,8 +1,10 @@
 import * as validator from 'validator';
 import { NextFunction, Request, Response } from 'express';
 import { HttpStatus } from './httpStatus';
-import { inspect } from './util';
+import { inspect } from 'util';
 import { logger } from './logger';
+import { NOTFOUND } from 'dns';
+import { fsync } from 'fs';
 
 class SharedRequestHandler {
     private static readonly SPACE = 2;
@@ -46,10 +48,82 @@ class SharedRequestHandler {
 
         next();
     }
-    jklhil
 
     validateUUID(_: Request, res: Response, next: NextFunction, id: any){
-        if (validator.isUUID(id))
+        if (validator.isUUID(id)){
+            logger.debug(`SharedRequestHandler.validateUUID(): isUUID`);
+            next();
+            return;
+        }
+
+        logger.debug(`SharedRequestHandler.validateUUID(): status= BAD_REQUEST`);
+        res.status(HttpStatus.BAD_REQUEST).send(
+            `${id} ist keine gueltige Spiel-ID`,
+        );
     }
 
+    validateContentType(req: Request, res: Response, next: NextFunction){
+        const contentType = req.header(`Content-Type`);
+        if(contentType === undefined || validator.isMimeType(contentType)){
+            logger.debug(`SharedRequestHandler.validateContentType(): ok`);
+            next();
+            return;
+        }
+
+        logger.debug(
+            `SharedRequestHandler.validateContentType(): status=BAD_REQUEST`,
+        );
+        res.status(HttpStatus.BAD_REQUEST).send(
+            `${contentType} ist kein gueltiger MIME-Typ`,
+        );
+    }
+
+    notFound(_: Request, res: Response ){
+        res.sendStatus(HttpStatus.NOT_FOUND);
+    }
+
+    internalError(err: any, _: Request, res: Response){
+        logger.error(
+            `SharedRequestHandler.internalError(): error=${inspect(err)}`,
+        );
+        res.sendStatus(HttpStatus.INTERNAL_ERROR);
+    }
+
+    notYetImplemented(_: Request, res: Response){
+        logger.error(`SharedRequestHandler.notYetImplemented()`);
+        res.sendStatus(HttpStatus.NOT_YET_IMPLEMENTED);
+    }
 }
+const handler = new SharedRequestHandler();
+//Folgende Functinen werden Exportiert
+
+export const logRequestHeader = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => handler.logRequestHeader(req, res, next);
+
+export const validateContentType = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => handler.validateContentType(req, res, next);
+
+export const validateUUID = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    id: any,
+) => handler.validateUUID(req, res, next, id);
+
+export const notFound = (req: Request, res: Response) =>
+    handler.notFound(req,res);
+
+export const internalError = (err: any, req: Request, res: Response) =>
+    handler.internalError(err, req, res);
+
+export const notYetImplemented = (req: Request, res: Response) =>
+    handler.notYetImplemented(req,res);
+
+export const wrap = (fn: any) => (...args: Array<any>) =>
+    fn(...args).catch(args[2]);
